a3dba2c70665340a9e0c968b3c0b145cbb90068f
Bailey Day <bailey.j.day@gmail.com>
2020-07-11 15:42:25 -0400

    Passing data between components

--- Practice 6/src/components/Ingredients/Ingredients.js
+++ Practice 6/src/components/Ingredients/Ingredients.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
+import IngredientList from "./IngredientList";
 
 const Ingredients = () => {
+  const addIngredientHandler = (ingredient) => {
+    setUserIngredients((prevIngredients) => [
+      ...prevIngredients,
+      { id: Math.random().toString, ingredient },
+    ]);
+  };
+
+  const [userIngredients, setUserIngredients] = useState([]);
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={userIngredients} />
       </section>
     </div>
   );
